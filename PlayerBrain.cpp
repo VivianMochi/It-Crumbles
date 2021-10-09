9565fa21b2a89d3ad484106ea7f089cef421a352
Nate Largo <nate954@vt.edu>
2021-10-08 22:55:38 -0400

    Add arrow keys as viable controls

--- PlayerBrain.cpp
+++ PlayerBrain.cpp
@@ -8,19 +8,19 @@ PlayerBrain::PlayerBrain() {
 }
 
 void PlayerBrain::update(sf::Time elapsed) {
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
 		entity->moveDirection.x = -1;
 	}
-	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
 		entity->moveDirection.x = 1;
 	}
 	else {
 		entity->moveDirection.x = 0;
 	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) || sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
 		entity->moveDirection.y = -1;
 	}
-	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) || sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
 		entity->moveDirection.y = 1;
 	}
 	else {
@@ -31,6 +31,6 @@ void PlayerBrain::update(sf::Time elapsed) {
 	// Aim direction is computed in the state that owns the robot
 
 	entity->fireControl = sf::Mouse::isButtonPressed(sf::Mouse::Left);
-	entity->jumpControl = sf::Keyboard::isKeyPressed(sf::Keyboard::Space) || sf::Keyboard::isKeyPressed(sf::Keyboard::LShift);
+	entity->jumpControl = sf::Keyboard::isKeyPressed(sf::Keyboard::Space) || sf::Keyboard::isKeyPressed(sf::Keyboard::LShift) || sf::Keyboard::isKeyPressed(sf::Keyboard::RShift);
 	entity->abilityControl = sf::Mouse::isButtonPressed(sf::Mouse::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::E);
 }
